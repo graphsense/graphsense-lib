f0b410c18fb693fb15f52b88eef60dbd9f29bade
Michael F. <michael.froewis@gmx.at>
2024-01-18 18:24:25 +0100

    enable deletes for diskcache, better keytype handling

--- tests/utils/test_tablecache.py
+++ tests/utils/test_tablecache.py
@@ -1,3 +1,4 @@
+import pytest
 from diskcache import Cache
 
 from graphsenselib.utils.cache import TableBasedCache
@@ -29,3 +30,14 @@ def test_cache():
     assert c.get_item("abc", 1) == [{"a": 1, "b": 2}, {"a": 1, "b": 3}]
     assert len(c.get_item("abc", 3)) == 1
     assert c.get_item("abc", 2) == 4
+
+    c.delete_item("abc", 1)
+    with pytest.raises(KeyError):
+        assert len(c.get_item("abc", 1)) == 2
+
+    assert c.get_item("abc", 2) == 4
+
+    del c[("abc", 2)]
+
+    with pytest.raises(KeyError):
+        c.get_item("abc", 2)
