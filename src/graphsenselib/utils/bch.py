78dabb780b0e19d0a46d3df72995cd86c2c209db
Michael F. <michael.froewis@gmx.at>
2025-06-24 14:01:34 +0200

    add cash address code

--- src/graphsenselib/utils/bch.py
+++ src/graphsenselib/utils/bch.py
@@ -1,4 +1,4 @@
-from cashaddress.convert import Address, to_legacy_address  # InvalidAddress
+from cashaddress.convert import Address, InvalidAddress, to_legacy_address
 
 # Patch P2SH32 legacy address support
 # https://bch.info/en/upgrade
@@ -10,3 +10,10 @@ if ("P2SH32", 11, False) not in Address.VERSION_MAP["cash"]:
 
 def bch_address_to_legacy(address):
     return to_legacy_address(address)
+
+
+def try_bch_address_to_legacy(address):
+    try:
+        return bch_address_to_legacy(address)
+    except InvalidAddress:
+        return address
