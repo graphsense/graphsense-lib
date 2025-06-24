21abe5367d8ecb3714e309f956987d95155c714a
Michael F. <michael.froewis@gmx.at>
2025-06-24 14:11:30 +0200

    harmonize with gsrest

--- src/graphsenselib/utils/accountmodel.py
+++ src/graphsenselib/utils/accountmodel.py
@@ -3,6 +3,12 @@ from typing import Optional, Union
 from .generic import remove_prefix
 
 
+def eth_address_to_hex(address):
+    if not isinstance(address, bytes):
+        return address
+    return "0x" + bytes_to_hex(address)
+
+
 def hex_str_to_bytes(hex_str: str) -> bytes:
     return bytes.fromhex(hex_str)
 
@@ -13,11 +19,19 @@ def bytes_to_hex(b: bytes) -> Optional[str]:
 
 
 def is_hex_string(string: Optional[str]) -> bool:
-    return string is not None and string.startswith("0x")
+    return (
+        string is not None
+        and (string.startswith("0x") or string.startswith("0X"))
+        and len(string) >= 2
+    )
 
 
 def strip_0x(string: Optional[str]) -> Optional[str]:
-    return remove_prefix(string, "0x") if is_hex_string(string) else string
+    return (
+        remove_prefix(remove_prefix(string, "0x"), "0X")
+        if is_hex_string(string)
+        else string
+    )
 
 
 def to_int(string: Union[str, int]) -> int:
