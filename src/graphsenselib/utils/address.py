d800ead4dc9931c5d40dd723a356e370dde2eea1
Michael F. <michael.froewis@gmx.at>
2025-06-24 14:25:01 +0200

    harmonize with gs-rest

--- src/graphsenselib/utils/address.py
+++ src/graphsenselib/utils/address.py
@@ -8,9 +8,19 @@ from bitarray import bitarray
 from bitarray.util import ba2int
 from cashaddress.convert import InvalidAddress as BCHInvalidAddress
 
-from .accountmodel import hex_to_bytes, strip_0x
-from .bch import bch_address_to_legacy
-from .tron import evm_to_tron_address_string, tron_address_to_evm_string
+from .accountmodel import (
+    eth_address_to_hex,
+    hex_str_to_bytes,
+    hex_to_bytes,
+    is_hex_string,
+    strip_0x,
+)
+from .bch import bch_address_to_legacy, try_bch_address_to_legacy
+from .tron import (
+    evm_to_tron_address_string,
+    tron_address_to_evm,
+    tron_address_to_evm_string,
+)
 
 
 class InvalidAddress(Exception):
@@ -279,3 +289,36 @@ def address_to_str(network: str, address: bytes) -> str:
         return c.to_str(address)
     else:
         raise ValueError("No address converter configured for network {network}")
+
+
+def cannonicalize_address(currency, address: str):
+    if currency == "trx":
+        return tron_address_to_evm(address, validate=False)
+    elif currency == "bch":
+        return try_bch_address_to_legacy(address)
+    elif currency == "eth":
+        return hex_str_to_bytes(strip_0x(address))
+    elif isinstance(address, str):
+        return address
+    else:
+        raise ValueError()
+
+
+def address_to_user_format(currency, db_address) -> str:
+    if currency == "eth":
+        if isinstance(db_address, bytes):
+            return eth_address_to_hex(db_address)
+        else:
+            return db_address.lower()
+    elif currency == "trx":
+        if isinstance(db_address, bytes):
+            return evm_to_tron_address_string(eth_address_to_hex(db_address))
+        else:
+            if is_hex_string(db_address):
+                return evm_to_tron_address_string(db_address)
+            else:
+                return db_address
+    elif isinstance(db_address, str):
+        return db_address
+    else:
+        raise Exception(f"Don't know how to decode db address, {db_address} {currency}")
--- tests/utils/test_address.py
+++ tests/utils/test_address.py
@@ -5,7 +5,13 @@ from functools import reduce
 
 import pytest
 
-from graphsenselib.utils.address import InvalidAddress, address_to_bytes, address_to_str
+from graphsenselib.utils.address import (
+    InvalidAddress,
+    address_to_bytes,
+    address_to_str,
+    address_to_user_format,
+    cannonicalize_address,
+)
 
 from . import resources
 
@@ -302,3 +308,28 @@ def test_address_converter_testset():
                         tests += 1
 
     assert tests > e_tests
+
+
+def test_address_conversions():
+    addr = "TAzsQ9Gx8eqFNFSKbeXrbi45CuVPHzA8wr"
+
+    assert (
+        cannonicalize_address("trx", addr).hex()
+        == "0b48984414cc0c6a8e599fb6e3bc11e599de2e24"
+    )
+
+    assert address_to_user_format("trx", cannonicalize_address("trx", addr)) == addr
+
+    assert address_to_user_format("trx", addr) == addr
+
+    eaddr = "0x0b48984414cc0c6a8e599fb6e3bc11e599de2e24"
+
+    assert address_to_user_format("eth", cannonicalize_address("eth", eaddr)) == eaddr
+
+    assert (
+        cannonicalize_address("eth", eaddr).hex()
+        == "0b48984414cc0c6a8e599fb6e3bc11e599de2e24"
+    )
+
+    with pytest.raises(ValueError):
+        cannonicalize_address("eth", addr).hex()
