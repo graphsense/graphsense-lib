77650b7fac0fcbda74e4b67cfcb57e2ed352e33a
Michael F. <michael.froewis@gmx.at>
2025-06-26 15:34:59 +0200

    add monitoring raw script to cli

--- src/graphsenselib/monitoring/cli.py
+++ src/graphsenselib/monitoring/cli.py
@@ -6,7 +6,7 @@ import click
 from ..cli.common import out_file, require_currency, require_environment
 from ..config import get_config
 from ..utils.console import console
-from .monitoring import DbSummaryRecord, get_db_summary_record
+from .monitoring import DbSummaryRecord, get_db_summary_record, is_raw_behind_schedule
 from .notifications import send_msg_to_topic
 
 
@@ -19,6 +19,52 @@ def monitoring_cli():
 def monitoring():
     """Tools to monitor the graphsense infrastructure."""
     pass
+
+
+@monitoring.command(
+    "monitor-raw-ingest",
+    short_help="checks if last data point is older than a threshold, optionally send slack notifications. ",
+)
+@require_environment()
+@require_currency(required=False)
+@click.option(
+    "--threshold", type=int, required=True, help="threshold in hours", default=12
+)
+@click.option(
+    "--topic",
+    "-t",
+    type=str,
+    help="Topic to send to.",
+    required=True,
+    default="exceptions",
+)
+@click.option("--dry-run/--no-dry-run", default=False)
+def monitor_raw_ingest(env, currency, threshold, topic, dry_run):
+    """Receives a summary record of the current database state.
+    \f
+
+    Args:
+        env (str): Env to work on
+        currency (str): currency to work on (optional)
+    """
+    config = get_config()
+    if currency is None:
+        # create rows for all configured currencies
+        records = [
+            (currency_config, is_raw_behind_schedule(env, currency_config, threshold))
+            for currency_config in config.get_environment(
+                env
+            ).get_configured_currencies()
+        ]
+    else:
+        records = [(currency, is_raw_behind_schedule(env, currency, threshold))]
+
+    for net, (bs, b, date) in records:
+        if bs:
+            msg = f"""WARNING: raw keyspace ingest {net} Last ingested block {(b,)} ({date}) older than {threshold} hours"""
+            print(msg)  # noqa T201
+            if not dry_run:
+                send_msg_to_topic(topic, msg)
 
 
 @monitoring.command(
