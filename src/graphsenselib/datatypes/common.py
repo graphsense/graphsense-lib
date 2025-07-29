a3eca25cd3829999d7e162297e690493f0bb26b0
Thomas Niedermayer <thomas.n71@gmail.com>
2025-07-29 08:26:08 +0200

    Move DB access from REST to gslib

--- src/graphsenselib/datatypes/common.py
+++ src/graphsenselib/datatypes/common.py
@@ -27,6 +27,14 @@ class FlowDirection(Enum):
         return str(self.value)
 
 
+class NodeType(Enum):
+    ADDRESS = "address"
+    CLUSTER = "cluster"
+
+    def __str__(self):
+        return self.value
+
+
 # TODO add as enums and remove from config
 # schema_types = ["utxo", "account"]
 # keyspace_types = ["raw", "transformed"]
