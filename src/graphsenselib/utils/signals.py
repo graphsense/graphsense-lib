3dc0b954158f6649d2d70b5714526d35c8d294b0
Thomas Niedermayer <thomas.n71@gmail.com>
2026-01-23 12:24:02 +0100

    Graceful shutdown handler: Also handle SIGTERM gracefully

--- src/graphsenselib/utils/signals.py
+++ src/graphsenselib/utils/signals.py
@@ -10,15 +10,16 @@ SHUTDOWN_SIGNAL_CNT = 0
 
 def shutdown_handler(signum, frame):
     global SHUTDOWN, SHUTDOWN_SIGNAL_CNT
-    logger.warning("Received STRG-C, will shutdown when reaching a safe spot.")
+    sig_name = "SIGINT" if signum == signal.SIGINT else "SIGTERM"
+    logger.warning(f"Received {sig_name}, will shutdown when reaching a safe spot.")
     SHUTDOWN = True
     SHUTDOWN_SIGNAL_CNT += 1
     if SHUTDOWN_SIGNAL_CNT > 10:
         logger.warning(
-            "Received STRG-C more than 10 times. "
+            "Received shutdown signal more than 10 times. "
             "Disabling shutdown handler. "
-            "The next STRG-C will terminate the process. "
-            "Be careful, you are on you own now."
+            "The next signal will terminate the process. "
+            "Be careful, you are on your own now."
         )
         unregister_ctrlc_shutdown_handler()
 
@@ -28,13 +29,15 @@ def shutdown_initialized():
 
 
 def register_ctrlc_shutdown_handler():
-    logger.info("Registered graceful ctrlc handler.")
+    logger.info("Registered graceful shutdown handler.")
     signal.signal(signal.SIGINT, shutdown_handler)
+    signal.signal(signal.SIGTERM, shutdown_handler)
 
 
 def unregister_ctrlc_shutdown_handler():
-    logger.info("Unregistered graceful ctrlc handler.")
+    logger.info("Unregistered graceful shutdown handler.")
     signal.signal(signal.SIGINT, signal.SIG_DFL)
+    signal.signal(signal.SIGTERM, signal.SIG_DFL)
 
 
 @contextmanager
