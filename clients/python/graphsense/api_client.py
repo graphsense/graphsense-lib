16a71fe53a6e2263443872ee9744dedd70bcd564
Michael F. <michael.froewis@gmx.at>
2026-06-18 18:34:55 +0200

    fix api version

--- clients/python/graphsense/api_client.py
+++ clients/python/graphsense/api_client.py
@@ -95,7 +95,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/2.13.5/python'
+        self.user_agent = 'graphsense-python/2.13.5'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):
